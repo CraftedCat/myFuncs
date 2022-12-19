040cd5d4225ba58a6ccc15e6c906482734436851
Test <test@test.com>
2022-12-19 17:15:44 +0300

    Init

---
+++ myFuncs.go
@@ -0,0 +1,36 @@
+package myFuncs
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func Exists(name string) (bool, error) {
+	_, err := os.Stat(name)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
+}
+
+func GetExist() []string {
+	var disksClear []string
+	disks := []string{"C:\\", "D:\\", "E:\\", "F:\\", "G:\\", "H:\\", "I:\\", "J:\\", "K:\\", "L:\\", "M:\\", "N:\\",
+		"O:\\", "P:\\", "Q:\\", "R:\\", "S:\\", "T:\\", "U:\\", "V:\\", "W:\\", "X:\\", "Y:\\", "Z:\\"}
+
+	for i := 0; i < len(disks); i++ {
+		disk := disks[i]
+		result, err := Exists(disk)
+		if err == nil {
+			if result {
+				disksClear = append(disksClear, disk)
+			}
+		}
+	}
+	fmt.Println(disksClear)
+	return disksClear
+}
